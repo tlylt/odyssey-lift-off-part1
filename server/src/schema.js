fcacf6daa6642a866594581e8378c10cabcf9d42
tlylt <liu_yongliang@hotmail.com>
2021-07-19 15:20:49 +0800

    Finish tutorial

---
+++ server/src/schema.js
@@ -0,0 +1,27 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  type Author {
+      id: ID!
+      name: String!
+      photo: String
+  }
+`;
+
+
+
+module.exports = typeDefs;
